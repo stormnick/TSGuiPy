20d0cc5267e4098e605fac25253f8c96c2b9ef93
Korboal <fifthrom@gmail.com>
2023-12-04 06:16:17 +0100

    adding home page

--- src/app.py
+++ src/app.py
@@ -1,3 +1,5 @@
+import configparser
+
 from flask import Flask, render_template, request, jsonify
 from src import plot
 
@@ -11,7 +13,7 @@ def cluster_run():
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('home_page.html')
 
 @app.route('/run_python', methods=['GET', 'POST'])
 def run_python():
@@ -35,6 +37,33 @@ def get_plot():
     fig = plot.create_plot()
     return jsonify({"data": fig.to_dict()["data"], "layout": fig.to_dict()["layout"]})
 
+@app.route('/config', methods=['GET', 'POST'])
+def config():
+    if request.method == 'POST':
+        config_data = {
+            'folderPath': request.form['folderPath'],
+            'filePath': request.form['filePath'],
+            'optionSelect': request.form['optionSelect'],
+            'numberInput': request.form['numberInput'],
+            'textInput': request.form['textInput']
+        }
+        save_config(config_data)
+        # Redirect or show a success message
+    else:
+        config_data = load_config()
+        # Render the form with config_data
+    return render_template('config.html', config=config_data)
+
+def save_config(config_data):
+    config = configparser.ConfigParser()
+    config['DEFAULT'] = config_data
+    with open('config.cfg', 'w') as configfile:
+        config.write(configfile)
+
+def load_config():
+    config = configparser.ConfigParser()
+    config.read('config.cfg')
+    return config['DEFAULT']
 
 
 if __name__ == '__main__':
