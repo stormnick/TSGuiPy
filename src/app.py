eb42b03c12157cd2e6d73069c84b65e0fa2af67c
Korboal <fifthrom@gmail.com>
2024-04-11 11:03:03 +0200

    hopefully fixed that it can run from terminal directly

--- src/app.py
+++ src/app.py
@@ -1,7 +1,11 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..',)))
+
 import configparser
 import importlib
 import json
-import os
 import sys
 import tempfile
 import zipfile
